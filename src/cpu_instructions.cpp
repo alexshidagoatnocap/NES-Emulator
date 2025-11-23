c0a0f8226ce0223377c7f43c2136d761c77d812b
alexshidagoatnocap <115957532+alexshidagoatnocap@users.noreply.github.com>
2025-11-23 03:24:41 -0600

    finished 2nd row of lut

--- src/cpu_instructions.cpp
+++ src/cpu_instructions.cpp
@@ -21,5 +21,20 @@ void Cpu6502::initInstructionLUT() {
   lut[0x0E] = {&c::IN_ASL, &c::MO_ABS, 6};
   lut[0x0F] = {&c::IN_WTF, &c::MO_IMP, 2};
 
-  lut[0x10] = {&c::IN_BPL, &c::MO_REL, 2};
+  lut[0x10] = {&c::IN_BPL, &c::MO_REL, 2}; // ** +1 if branch
+  lut[0x11] = {&c::IN_ORA, &c::MO_IDY, 5}; // *  +1 if page bound crossed
+  lut[0x12] = {&c::IN_WTF, &c::MO_IMP, 2};
+  lut[0x13] = {&c::IN_WTF, &c::MO_IMP, 2};
+  lut[0x14] = {&c::IN_WTF, &c::MO_IMP, 2};
+  lut[0x15] = {&c::IN_ORA, &c::MO_ZPX, 4};
+  lut[0x16] = {&c::IN_ASL, &c::MO_ZPX, 6};
+  lut[0x17] = {&c::IN_WTF, &c::MO_IMP, 2};
+  lut[0x18] = {&c::IN_CLC, &c::MO_IMP, 2};
+  lut[0x19] = {&c::IN_ORA, &c::MO_ABY, 4}; // *
+  lut[0x1A] = {&c::IN_WTF, &c::MO_IMP, 2};
+  lut[0x1B] = {&c::IN_WTF, &c::MO_IMP, 2};
+  lut[0x1C] = {&c::IN_WTF, &c::MO_IMP, 2};
+  lut[0x1D] = {&c::IN_ORA, &c::MO_ABX, 4}; // *
+  lut[0x1E] = {&c::IN_ASL, &c::MO_ABX, 7};
+  lut[0x1F] = {&c::IN_WTF, &c::MO_IMP, 2};
 }
